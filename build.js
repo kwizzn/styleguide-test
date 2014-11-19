dffb5e01a1f25c288ec0425e7d24ba9b867619ce
Christian Neuhäuser <chris@type10.com>
2014-11-19 12:59:53 +0100

    Removes metalsmith-handlebars-content for handlebars-content & adds a custom partials configuration

--- build.js
+++ build.js
@@ -1,12 +1,12 @@
 'use strict';
 
+var fs          = require('fs');
 var path        = require('path');
 var glob        = require('glob');
 var Handlebars  = require('handlebars');
 var metalsmith  = require('metalsmith');
 var kss         = require('metalsmith-kss');
 var markDown    = require('metalsmith-markdown');
-var hbs         = require('metalsmith-handlebars-content');
 var navigation  = require('metalsmith-navigation');
 var templates   = require('metalsmith-templates');
 var assets      = require('metalsmith-assets');
@@ -29,20 +29,35 @@ var navSettings = {
 var meta = {
     title: 'Potec',
     description: 'Styleguide',
-    // used by metalsmith-templates
-    partials: {
+    partials: { // Used by metalsmith-templates
         breadcrumbs: '_breadcrumbs',
-
         nav_global : '_nav_global',
         nav_relative : '_nav_relative',
         nav_footer : '_nav_footer',
-
         nav__children: '_nav__children'
     }
 };
 
-var relativePathHelper = function(current, target) {
-    // normalize and remove starting slash from path
+/**
+ * Registers a Handlebars partial for all matches in the given directory
+ */
+function getModulePartials(directory, callback) {
+    directory = directory || [];
+    callback = callback || function() {};
+    directory.forEach(function(item, i) {
+        fs.readFile(item, { encoding: 'utf8' }, function(err, content) {
+            Handlebars.registerPartial(item, content);
+            if (i + 1 >= directory.length) {
+                callback();
+            }
+        });
+    });
+}
+
+/**
+ * Normalizes and removes starting slash from path
+ */
+function getRelativePath(current, target) {
     if(!current || !target){
         return '';
     }
@@ -50,17 +65,17 @@ var relativePathHelper = function(current, target) {
     target = path.normalize(target).slice(0);
     current = path.dirname(current);
     return path.relative(current, target);
-};
+}
 
-Handlebars.registerHelper('relative_path', relativePathHelper);
+Handlebars.registerHelper('relative_path', getRelativePath);
 
 metalsmith(__dirname)
     .clean(true)
     .metadata(meta)
     .use(kss({ source: 'less/', target: 'styleguide/', pageTemplate: 'page.html', fixtures: glob.sync('less/**/*.hbs') }))
     .use(markDown())
-    .use(hbs({ Handlebars: Handlebars }))
     .use(navigation(navConfigs, navSettings))
+    .use(templates({ engine: 'handlebars', directory: 'src/prototyp', inPlace: true, partials: getModulePartials(glob.sync('modules/**/*.html')) }))
     .use(templates({ engine: 'handlebars' }))
     .use(assets({ source: './assets', destination: './assets' }))
     .build(function(err) {
